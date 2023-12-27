634ae52cca30f2cee1c92d52b2c13bb8472cf6bf
Mohamed-Inthisham <balck.devil9b@gmail.com>
2023-12-27 20:15:46 +0530

    employee features added

--- src/main/java/lk/trendz/Employee_Management/service/EmployeeService.java
+++ src/main/java/lk/trendz/Employee_Management/service/EmployeeService.java
@@ -4,7 +4,11 @@ import lk.trendz.Employee_Management.controller.request.EmployeeRequest;
 import lk.trendz.Employee_Management.controller.response.EmployeeResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface EmployeeService {
     EmployeeResponse create(EmployeeRequest employeeRequest);
+    List<EmployeeResponse> getAll(EmployeeRequest employeeRequest);
+    EmployeeResponse specificEmployeeDetails(Long id);
 }
