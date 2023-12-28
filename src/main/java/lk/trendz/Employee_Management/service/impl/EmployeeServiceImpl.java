88a5520afef5d77738a8756af27d3afd9eead55a
Mohamed-Inthisham <balck.devil9b@gmail.com>
2023-12-28 10:52:18 +0530

    employee update and delete feature

--- src/main/java/lk/trendz/Employee_Management/service/impl/EmployeeServiceImpl.java
+++ src/main/java/lk/trendz/Employee_Management/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package lk.trendz.Employee_Management.service.impl;
 
 import lk.trendz.Employee_Management.controller.request.EmployeeRequest;
 import lk.trendz.Employee_Management.controller.response.EmployeeResponse;
+import lk.trendz.Employee_Management.controller.response.OutputMessageResponse;
 import lk.trendz.Employee_Management.model.Employee;
 import lk.trendz.Employee_Management.repository.EmployeeRepository;
 import lk.trendz.Employee_Management.service.EmployeeService;
@@ -77,6 +78,45 @@ public class EmployeeServiceImpl implements EmployeeService {
             employeeResponse.setMaritalStatus(employee.getMaritalStatus());
             employeeResponse.setEmail(employee.getEmail());
             employeeResponse.setContactNumber(employee.getContactNumber());
+            return employeeResponse;
+        }
+        return null;
+    }
+    @Override
+    public OutputMessageResponse delete(Long id) {
+        employeeRepository.deleteById(id);
+        OutputMessageResponse outputResponse=new OutputMessageResponse();
+        outputResponse.setMessage("Employee Deleted Successfully ");
+        outputResponse.setEmpId(id);
+        return outputResponse;
+    }
+    @Override
+    public EmployeeResponse update(Long id, EmployeeRequest employeeRequest) {
+        Optional<Employee> optionalEmployee=employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()){
+            Employee employee=optionalEmployee.get();
+            employee.setFirstName(employeeRequest.getFirstName());
+            employee.setLastName(employeeRequest.getLastName());
+            employee.setDob(employeeRequest.getDob());
+            employee.setGender(employeeRequest.getGender());
+            employee.setNationality(employeeRequest.getNationality());
+            employee.setAddress(employeeRequest.getAddress());
+            employee.setMaritalStatus(employeeRequest.getMaritalStatus());
+            employee.setEmail(employeeRequest.getEmail());
+            employee.setContactNumber(employeeRequest.getContactNumber());
+
+            Employee updateEmployee=employeeRepository.save(employee);
+
+            EmployeeResponse employeeResponse=new EmployeeResponse();
+            employeeResponse.setFirstName(updateEmployee.getFirstName());
+            employeeResponse.setLastName(updateEmployee.getLastName());
+            employeeResponse.setDob(updateEmployee.getDob());
+            employeeResponse.setGender(updateEmployee.getGender());
+            employeeResponse.setNationality(updateEmployee.getNationality());
+            employeeResponse.setAddress(updateEmployee.getAddress());
+            employeeResponse.setMaritalStatus(updateEmployee.getMaritalStatus());
+            employeeResponse.setEmail(updateEmployee.getEmail());
+            employeeResponse.setContactNumber(updateEmployee.getContactNumber());
             return employeeResponse;
         }
         return null;
